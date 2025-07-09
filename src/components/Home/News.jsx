3db2ec04f2a9af88d6a1638216663a48b4861bd5
sokratgruzit <tavadzed@gmail.com>
2025-07-09 20:13:42 +0400

    last

--- src/components/Home/News.jsx
+++ src/components/Home/News.jsx
@@ -6,56 +6,33 @@ import styles from "./Home.module.css";
 const News = () => {
     const { t } = useTranslation("common");
 
-    const newsData = [
-        {
-            src: "/gallery/0.webp",
-            title: t("promo.newsTitle"),
-            descr: t("promo.newsText"),
-            date: "23.2.25"
-        },
-        {
-            src: "/gallery/1.webp",
-            title: t("promo.newsTitle"),
-            descr: t("promo.newsText"),
-            date: "23.2.25"
-        },
-        {
-            src: "/gallery/2.webp",
-            title: t("promo.newsTitle"),
-            descr: t("promo.newsText"),
-            date: "23.2.25"
-        }
-    ];
+    const t1 = t("news.1");
+    const t2 = t("news.2");
+    const t3 = t("news.3");
+    const t4 = t("news.4");
+    const t5 = t("news.5");
+    const t6 = t("news.6");
 
     return (
         <div className={styles.newsContainer}>
-            <motion.div 
-                className={styles.titleWrap}
+            <motion.h1 
+                className={`${styles.bannerTitle} h1 text-shadow-red`}
                 initial={{ translateY: -50, opacity: 0 }}
                 whileInView={{ translateY: 0, opacity: 1 }}
                 transition={{ duration: 0.8, ease: "easeInOut", delay: 0.1 }}
             >
-                <h1 className="h1 text-shadow" style={{ color: "white", textAlign: "start" }}>{t("promo.news")}</h1>
-            </motion.div>
+                {t1}
+            </motion.h1>
+            <p className={styles.newsDescr}>{t2}</p>
             <div className={styles.newsWrap}>
-                {newsData.map((item, i) => {
-                    return (
-                        <motion.div 
-                            key={i} 
-                            className={styles.newsItem}
-                            initial={{ translateY: 50, opacity: 0 }}
-                            whileInView={{ translateY: 0, opacity: 1 }}
-                            transition={{ duration: 0.5, ease: "easeOut", delay: 0.2 * (i + 1)}}
-                        >
-                            <img src={item.src} alt="news image" />
-                            <div className={styles.textBlock}>
-                                <h2>{item.title}</h2>
-                                <p>{item.descr}</p>
-                                <span>{item.date}</span>
-                            </div>
-                        </motion.div>
-                    )
-                })}
+                <div className={styles.newsItem}>
+                    <h2>{t3}</h2>
+                    <p>{t4}</p>
+                </div>
+                <div className={styles.newsItem}>
+                    <h2>{t5}</h2>
+                    <p dangerouslySetInnerHTML={{ __html: t6 }} />  
+                </div>
             </div>
         </div>
     )
